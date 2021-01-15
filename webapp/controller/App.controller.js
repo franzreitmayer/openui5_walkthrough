6011d654958715385f13ed5bc77df7848e92a948
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2021-01-15 20:20:21 +0100

    Step 09

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,30 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (Controller, MessageToast, JSONModel, ResourceModel) {
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("eu.reitmayer.openui5.walkthrough.controller.App", {
-        onInit: function () {
-            // set data model on view
-            var oData = {
-                recipient: {
-                    name: "World"
-                }
-            };
-            var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-
-            // set i18n model on view
-            var i18nModel = new ResourceModel({
-                bundleName: "eu.reitmayer.openui5.walkthrough.i18n.i18n",
-                supportedLocales: [""],
-                fallbackLocale: ""
-            });
-            this.getView().setModel(i18nModel, "i18n");
-
-        },
         onShowHello: function () {
             // read msg from i18n model
             var oBundle = this.getView().getModel("i18n").getResourceBundle();
