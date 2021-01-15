43d49b81208582bd7d59c05946848081afc23b23
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2021-01-15 19:30:33 +0100

    Step 08

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,37 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
- ], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("eu.reitmayer.openui5.walkthrough.controller.App", {
-        onShowHello : function () {
-           MessageToast.show("Hello World!");
+        onInit: function () {
+            // set data model on view
+            var oData = {
+                recipient: {
+                    name: "World"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+
+            // set i18n model on view
+            var i18nModel = new ResourceModel({
+                bundleName: "eu.reitmayer.openui5.walkthrough.i18n.i18n",
+                supportedLocales: [""],
+                fallbackLocale: ""
+            });
+            this.getView().setModel(i18nModel, "i18n");
+
+        },
+        onShowHello: function () {
+            // read msg from i18n model
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            // show message
+            MessageToast.show(sMsg);
         }
-     });
- });
+    });
+});
