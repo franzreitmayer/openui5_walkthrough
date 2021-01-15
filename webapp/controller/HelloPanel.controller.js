c89502464d72e0649db8a4fe89c67ab70f91bdb1
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2021-01-15 22:56:29 +0100

    Step 16: Dialogs and Fragments

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
- ], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
+ ], function (Controller, MessageToast, Fragment) {
     "use strict";
     return Controller.extend("eu.reitmayer.openui5.walkthrough.controller.HelloPanel", {
        onShowHello : function () {
@@ -11,6 +12,24 @@ sap.ui.define([
           var sMsg = oBundle.getText("helloMsg", [sRecipient]);
           // show message
           MessageToast.show(sMsg);
+       },
+       onOpenDialog : function () {
+           var oView = this.getView();
+
+           // create dialog lazily
+           if (!this.pDialog) {
+               this.pDialog = Fragment.load({
+                   id: oView.getId(),
+                   name: "eu.reitmayer.openui5.walkthrough.view.HelloDialog"
+               }).then(function (oDialog) {
+                   // connect dialog to the root view of this component (models, lifecycle)
+                   oView.addDependent(oDialog);
+                   return oDialog;
+               });
+           } 
+           this.pDialog.then(function(oDialog) {
+               oDialog.open();
+           });
        }
     });
  });
